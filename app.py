b61726bca3071ac229f439d753252624b42c89db
Dana Sharon <whosedreamisthis@gmail.com>
2026-07-18 22:53:41 -0700

    allow cors in app.py, initialized nextjs app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from src.predict import get_readable_recommendations
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
